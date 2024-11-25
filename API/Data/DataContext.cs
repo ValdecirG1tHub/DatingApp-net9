e7319dddffcf274c4694e83dcd26d8b7ec7886a3
ValdecirG1tHub <valdecirgithub@gmail.com>
2024-11-25 20:41:38 -0300

    End of Section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public required DbSet<AppUser> Users { get; set; }
+}
